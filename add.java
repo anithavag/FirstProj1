a543657da0829951adec49fdd68fa89423a482d3
anithavag <anithavaganan@gmail.com>
2024-08-15 14:02:52 +0530

    Sub Function added

--- add.java
+++ add.java
@@ -2,10 +2,13 @@ class add{
 public static void main(String[] args) {
     
     int a =1;
-    int b=2;
+    int b=5;
     int c=a+b;
-System.out.println("Sum: " +c);
 
+    int d= b-a;
+
+System.out.println("Sum: " +c);
+System.out.println("Diff": " +d);
 }
 
 }
